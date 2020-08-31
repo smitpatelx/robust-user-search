c22212199b3ade1e9ee533ffb16228cf94c182ab
Smit Patel <smitbpatel1999@gmail.com>
2020-08-31 12:59:21 -0500

    Re Sync Functionality Added

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -2,9 +2,9 @@
     <div class="w-full flex flex-wrap">
         <div class="w-full mt-2 flex">
             <input @keydown.esc="search_text=''" class="text-base bg-blue-500 placeholder-text-gray-200" type="text" placeholder="Search User" v-model="search_text" title="Press Esc to clear !">
-            <button @click.prevent="searchUser" class="bg-primary hover:bg-gray-700 transition-colors duration-500 text-white px-2 ml-2 rounded flex flex-wrap justify-center items-center focus:outline-none focus:shadow-outline">
+            <button @click="reSync" class="bg-primary hover:bg-gray-700 transition-colors duration-500 text-white pl-2 pr-3 ml-2 rounded flex flex-wrap justify-center items-center focus:outline-none focus:shadow-outline">
                 <svg class="w-4 h-4 fill-current inline-block text-white mr-2" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M13.4 1c.4.3.6.7.6 1.1L13 9H21a1 1 0 01.8 1.6l-10 12A1 1 0 0110 22L11 15H3a1 1 0 01-.8-1.6l10-12a1 1 0 011.2-.3zM5.1 13H12a1 1 0 011 1.1l-.6 4.6L19 11H12a1 1 0 01-1-1.1l.6-4.6L5 13z" clip-rule="evenodd"/></svg> 
-                <span class="text-base">Search</span>
+                <span class="text-base">ReSync</span>
             </button>
             <select @change="getNewRoles" v-model="currentRole" name="role" class="w-40 ml-10">
                 <option value="" default >All Roles</option>
@@ -34,12 +34,12 @@
                         <div class="flex flex-wrap w-4/5">
                             <span v-for="(rl,x) in user.roles" :key='x' class="rounded-full bg-gray-600 text-white py-1 px-3 text-sm ml-2 my-1"> {{rl}}</span>
                         </div>
-                        <a :href="'/wp-admin/user-edit.php?user_id='+user.id+'&wp_http_referer=%2Fwp-admin%2Fusers.php'"  target="_blank" class="w-1/5 text-teal-400 hover:text-teal-600">
-                            <svg class="w-5 h-5 fill-current inline-block mr-2" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M5 7a1 1 0 00-1 1v11a1 1 0 001 1h11a1 1 0 001-1v-6a1 1 0 112 0v6a3 3 0 01-3 3H5a3 3 0 01-3-3V8a3 3 0 013-3h6a1 1 0 110 2H5zM14 3c0-.6.4-1 1-1h6c.6 0 1 .4 1 1v6a1 1 0 11-2 0V4h-5a1 1 0 01-1-1z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M21.7 2.3c.4.4.4 1 0 1.4l-11 11a1 1 0 01-1.4-1.4l11-11a1 1 0 011.4 0z" clip-rule="evenodd"/></svg>
+                        <a :href="'/wp-admin/user-edit.php?user_id='+user.id+'&wp_http_referer=%2Fwp-admin%2Fusers.php'"  target="_blank" class="w-1/5 text-teal-400 hover:text-teal-600 visited:text-indigo-500 focus:outline-none select-none">
+                            <svg class="w-5 h-5 fill-current inline-block mr-2 focus:outline-none select-none" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M5 7a1 1 0 00-1 1v11a1 1 0 001 1h11a1 1 0 001-1v-6a1 1 0 112 0v6a3 3 0 01-3 3H5a3 3 0 01-3-3V8a3 3 0 013-3h6a1 1 0 110 2H5zM14 3c0-.6.4-1 1-1h6c.6 0 1 .4 1 1v6a1 1 0 11-2 0V4h-5a1 1 0 01-1-1z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M21.7 2.3c.4.4.4 1 0 1.4l-11 11a1 1 0 01-1.4-1.4l11-11a1 1 0 011.4 0z" clip-rule="evenodd"/></svg>
                         </a>
                     </span>
                 </div>
-                <div v-if="search_arr.length==0" class="text-base w-full text py-3 flex items-center justify-center text-red-500 bg-red-100 font-medium">
+                <div v-if="search_arr.length==0" class="text-base w-full text py-3 flex items-center justify-center text-red-500 bg-red-100 font-medium focus:outline-none select-none">
                     <span class="pl-4 pr-2 py-1 text-center flex-1">No Data Found. Try different search.</span>
                 </div>
             </div>
@@ -61,11 +61,11 @@ export default {
     methods:{
         async getAllUsers(){
             let res = await axios({
-                method: 'get',
-                url: `/wp-json/rsu/v1/all`,
-                headers: {
-                    'X-WP-Nonce':rusN.nonce
-                }
+            method: 'get',
+            url: `/wp-json/rsu/v1/all`,
+            headers: {
+                'X-WP-Nonce':rusN.nonce
+            }
             });
             this.users = res.data;
             this.search_arr = res.data;
@@ -102,7 +102,6 @@ export default {
                 }
             });
             this.roles = res.data;
-            console.log(res.data)
         },
         slug(str){
             str = str.replace(/^\s+|\s+$/g, ''); // trim
@@ -121,7 +120,7 @@ export default {
 
             return str;
         },
-        async searchUser(){
+        async searchUser(val){
             let pattern = new RegExp(val.toString().trim().toLowerCase(),"g")
             let search_arr = await this.users.filter(function(arr,index){
                 if(arr.username.toString().trim().toLowerCase().match(pattern) || 
@@ -136,24 +135,32 @@ export default {
                 }   
             });
             this.search_arr = search_arr;
+        },
+        async reSync(){
+            await new Promise(async (resolve, reject)=>{
+                let res = await axios({
+                method: 'get',
+                url: `/wp-json/rsu/v1/all`,
+                headers: {
+                    'X-WP-Nonce':rusN.nonce
+                }
+                });
+                this.users = res.data;
+                this.search_arr = res.data;
+                
+                if(res.data.length == this.search_arr.length){
+                    resolve("Success fetching data")
+                } else {
+                    reject("Data fetching failed")
+                }
+            }).then(()=>{
+                this.searchUser(this.search_text)
+            })
         }
     },
     watch:{
         search_text(val){
-            let pattern=new RegExp(val.toString().trim().toLowerCase(),"g")
-            let search_arr = this.users.filter(function(arr,index){
-                if(arr.username.toString().trim().toLowerCase().match(pattern) || 
-                    arr.first_name.toString().trim().toLowerCase().match(pattern) ||
-                    arr.last_name.toString().trim().toLowerCase().match(pattern) ||
-                    arr.billing_company.toString().trim().toLowerCase().match(pattern) ||
-                    arr.email.toString().trim().toLowerCase().match(pattern)
-                    ){
-                    return true;
-                }else{
-                    return false;
-                }   
-            });
-            this.search_arr = search_arr;
+            this.searchUser(val);
         }
     },
     mounted(){
@@ -168,6 +175,17 @@ export default {
 }
 </script>
 <style lang="scss">
+    .focus\:outline-none{
+        outline: 0 !important;
+        outline: none !important;
+        @apply shadow-none;
+        &:focus{
+            outline: 0 !important;
+            outline: none !important;
+            @apply shadow-none;
+        }
+    }
+
     .custom_scroll{
         &::-webkit-scrollbar-track
         {
