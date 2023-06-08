96a030bf3bba72e5910987c72e2d1e4882c4fe4f
smitpatelx <smitbpatel1999@gmail.com>
2023-06-07 19:36:58 -0500

    Readme and version update.

--- robust-user-search.php
+++ robust-user-search.php
@@ -6,7 +6,7 @@
  * Text Domain:         robust-user-search
  * Author:              Smit Patel
  * Author URI:          https://smitpatelx.com
- * Version:             1.0.6
+ * Version:             1.1.0
  * Requires at least:   5.2
  * Requires PHP:        7.1
  * License:             gpl-v2-only
