89799d2dc8f6aca84513ba7ee455126386424501
smitpatelx <smitbpatel1999@gmail.com>
2023-07-16 05:28:27 -0500

    Now list all users can filter by company name, fitst name, last name, email, username, role & phone number.

--- api/list-all-users.php
+++ api/list-all-users.php
@@ -43,34 +43,77 @@ class RusRestApiGetAllUsers {
         RusHelper::checkNonceApi($request);
         
         extract($request->get_params());
-        $DBRecord = array();
-        $args = array(
-            'orderby' => $sort !== null ? $sort_by : 'user_registered',
-            'order'   => $sort,
-            'role' => isset($role) ? $role : null,
-        );
+        $DBRecord = [];
 
-        $users = get_users( $args );
+        // Pagination
+        $page = isset($page) ? $page : 1;
+        $page_size = isset($page_size) ? $page_size : 10;
+        $offset = ($page - 1) * $page_size;
+
+        // Sorting
+        $sort_order = isset($sort_order) ? $sort_order : 'asc';
+        $sort_by = self::filterIsSetNull($sort_by);
+
+        // Search
+        $search_text = self::filterNull($search_text);
+
+        // Role
+        $role = self::filterNull($role);
+
+        global $wpdb;
+
+        $sql = "
+            SELECT * FROM {$wpdb->users}
+            INNER JOIN {$wpdb->usermeta} ON ({$wpdb->users}.ID = {$wpdb->usermeta}.user_id)
+            WHERE {$wpdb->users}.ID LIKE \"%$search_text%\" OR {$wpdb->users}.user_login LIKE \"%$search_text%\"
+            OR {$wpdb->users}.user_email LIKE \"%$search_text%\" OR {$wpdb->users}.user_nicename LIKE \"%$search_text%\"
+            OR {$wpdb->users}.display_name LIKE \"%$search_text%\" OR {$wpdb->usermeta}.meta_value LIKE \"%$search_text%\"
+            GROUP BY {$wpdb->users}.ID
+            ORDER BY {$wpdb->users}.ID $sort_order
+            LIMIT $offset, $page_size
+        ";
+
+        $users = $wpdb->get_results($sql);
+
+        $DBRecord['total'] = count($users);
+        $DBRecord['page'] = $page;
+        $DBRecord['page_size'] = $page_size;
+        $DBRecord['users'] = array();
         $i=0;
 
         foreach ( $users as $user )
         {
-            $DBRecord[$i]['roles']                  = self::filterNull($user->roles);
-            $DBRecord[$i]['username']               = self::filterNull($user->user_login);
-            $DBRecord[$i]['id']                     = self::filterNull($user->ID);
-            $DBRecord[$i]['first_name']             = self::filterNull($user->first_name);
-            $DBRecord[$i]['last_name']              = self::filterNull($user->last_name);
-            $DBRecord[$i]['user_registered']        = self::filterNull($user->user_registered);
-            $DBRecord[$i]['email']                  = self::filterNull($user->user_email);
+            $record = array();
+            $record['roles']                  = self::filterNull($user->roles);
+            $record['username']               = self::filterNull($user->user_login);
+            $record['id']                     = self::filterNull($user->ID);
+            $record['user_registered']        = self::filterNull($user->user_registered);
+            $record['email']                  = self::filterNull($user->user_email);
 
             $UserData = get_user_meta( $user->ID );  
-            $DBRecord[$i]['billing_company']        = self::filterNull($UserData['billing_company'][0]);
-            $DBRecord[$i]['billing_address_1']      = self::filterNull($UserData['billing_address_1'][0]);
-            $DBRecord[$i]['billing_city']           = self::filterNull($UserData['billing_city'][0]);
-            $DBRecord[$i]['billing_state']          = self::filterNull($UserData['billing_state'][0]);
-            $DBRecord[$i]['billing_postcode']       = self::filterNull($UserData['billing_postcode'][0]);
-            $DBRecord[$i]['billing_country']        = self::filterNull($UserData['billing_country'][0]);
-            $DBRecord[$i]['billing_phone']          = self::filterNull($UserData['billing_phone'][0]);
+            $record['roles']             = self::filterNull($UserData['wp_capabilities'][0]);
+            
+            // https://regex101.com/library/3q3RYF - smit
+            // a:1:{s:11:"contributor";b:1;} ==to==> ["contributor"]
+            $re = '/"([^"]+)"/';
+            preg_match_all($re, $record['roles'], $matches, PREG_SET_ORDER, 0);
+            if ($matches) {
+                $record['roles'] = [];
+                foreach ($matches as $key => $value) {
+                    array_push($record['roles'], $value[1]);
+                }
+            }
+
+            $record['first_name']             = self::filterNull($UserData['first_name'][0]);
+            $record['last_name']              = self::filterNull($UserData['last_name'][0]);
+            $record['billing_company']        = self::filterNull($UserData['billing_company'][0]);
+            $record['billing_address_1']      = self::filterNull($UserData['billing_address_1'][0]);
+            $record['billing_city']           = self::filterNull($UserData['billing_city'][0]);
+            $record['billing_state']          = self::filterNull($UserData['billing_state'][0]);
+            $record['billing_postcode']       = self::filterNull($UserData['billing_postcode'][0]);
+            $record['billing_country']        = self::filterNull($UserData['billing_country'][0]);
+            $record['billing_phone']          = self::filterNull($UserData['billing_phone'][0]);
+            $DBRecord['users'][$i] = $record;
             $i++; 
         }
         return new \WP_REST_Response($DBRecord, 200);
